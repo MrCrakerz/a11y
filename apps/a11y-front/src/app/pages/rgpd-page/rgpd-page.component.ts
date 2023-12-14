68f0ad47c686416f100887eeffc061e97d314deb
Mohammed <mohammed.berguella@ynov.com>
2023-12-14 10:31:17 +0100

    RGPD page

--- apps/a11y-front/src/app/pages/rgpd-page/rgpd-page.component.ts
+++ apps/a11y-front/src/app/pages/rgpd-page/rgpd-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HeaderComponent} from "../../components/header/header.component";
 import {FooterComponent} from "../../components/footer/footer.component";
