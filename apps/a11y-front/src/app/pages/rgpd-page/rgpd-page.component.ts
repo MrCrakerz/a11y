8190ddba75de73a6e785dccaf9681db25b956841
Mohammed <mohammed.berguella@ynov.com>
2023-12-14 10:30:49 +0100

    RGPD page

---
+++ apps/a11y-front/src/app/pages/rgpd-page/rgpd-page.component.ts
@@ -0,0 +1,17 @@
+import {Component, inject} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HeaderComponent} from "../../components/header/header.component";
+import {FooterComponent} from "../../components/footer/footer.component";
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'a11y-rgpd-page',
+  standalone: true,
+  imports: [CommonModule, FormsModule, HeaderComponent, FooterComponent],
+  templateUrl: './rgpd-page.component.html',
+  styleUrls: ['./rgpd-page.component.scss'],
+})
+
+export class RgpdPageComponent {
+
+}
